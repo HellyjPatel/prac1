82fd138fc4dba5a7d157b4c2bee2e48b7b15066a
HellyjPaatel <hellypatel221202@gmail.com>
2022-08-17 17:10:21 +0530

    h

---
+++ src/main/kotlin/Type_conversion.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    val n: Int = 10
+    val d: Double = n.toDouble()
+    println("Integer value: $n")
+    println("Double value: $d")
+
+    val s: String = "10"
+    val i: Int = s.toInt()
+    println("String value: $s")
+    println("Double value (From string): $i")
+
+    val d1: Double = s.toDouble()
+    println("Double value (From string): $d1")
+}
