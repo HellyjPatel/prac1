6382540e98645449494ef2efa80b5892b7a2e9fc
HellyjPaatel <hellypatel221202@gmail.com>
2022-08-23 18:28:27 +0530

    h

--- src/main/kotlin/q7.kt
+++ src/main/kotlin/q7.kt
@@ -1,5 +1,18 @@
-fun main(){
-    println("Enter no to find it's Factorial = ")
-    val no: Int = readLine()!!.toInt()
-    println("Factorial of $no is: "Fact(no))
+fun main() {
+    print("Enter number:")
+    val input = readLine()!!
+    val num : Int = input.toInt()
+    val ans : Long = getfact(num)
+    print(ans)
+}
+tailrec fun getfact(num:Int,last:Int=1) : Long
+{
+    return if (num == 0)
+    {
+        last.toLong()
+    }
+    else
+    {
+        getfact(num-1,last*num)
+    }
 }
